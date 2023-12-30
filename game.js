418945f75dc2e1eae70367a43080f91615b293e9
Benjamin-Bailes <139053033+Benjamin-Bailes@users.noreply.github.com>
2023-12-30 14:34:12 +1030

    misc

--- game.js
+++ game.js
@@ -1,10 +1,49 @@
-const player1 = document.getElementById("player1");
+// const player1 = document.getElementById("player1");
 const button1 = document.getElementById("button1");
 const gameWindow = document.getElementById("game-window");
 
 const gameWindowRect = gameWindow.getBoundingClientRect();
-let player1Y = player1.getBoundingClientRect().top;
-let player1X = player1.getBoundingClientRect().left;
+// let player1Y = player1.getBoundingClientRect().top;
+// let player1X = player1.getBoundingClientRect().left;
+
+
+class Player {
+  constructor(){
+    this.div = document.getElementById("player1");
+    this.boundries = {
+      top: false,
+      bottom: false,
+      left: false,
+      right: false
+    }
+  }
+
+  // methods.
+  getXPos(){
+    return this.div.getBoundingClientRect().left;
+  }
+  getYPos(){
+    return this.div.getBoundingClientRect().top;
+  }
+
+  incrementPos(dx,dy){
+    let x = this.getXPos();
+    let y = this.getYPos();
+    this.div.style.left = `${x+dx}px`;
+    this.div.style.top = `${y+dy}px`;
+  }
+
+  checkBoundries(){
+
+  }
+}
+
+var player1 = new Player();
+
+// console.log(player1.div);
+
+
+
 
 const keysPressed = {
   ArrowUp: false,
@@ -22,14 +61,14 @@ const playerBoundries = {
 // listen for keys state.
 document.addEventListener("keydown", handleKeyDown);
 document.addEventListener("keyup", handleKeyUp);
-button1.addEventListener("click", ()=>{
-  if(button1.textContent === "pause"){
-    button1.textContent = "resume"; // not playing
-  } else {
-    button1.textContent = "pause"; // playing
-    movePlayer();
-  }
-})
+// button1.addEventListener("click", ()=>{
+//   if(button1.textContent === "pause"){
+//     button1.textContent = "resume"; // not playing
+//   } else {
+//     button1.textContent = "pause"; // playing
+//     movePlayer();
+//   }
+// })
 
 function handleKeyDown(event){
   keysPressed[event.key] = true;
@@ -40,88 +79,65 @@ function handleKeyUp(event){
   // movePlayer();
 }
 
-function checkBoundries(){
-  let player1Rect = player1.getBoundingClientRect();
-  if(player1Rect.right >= gameWindowRect.right){
-    playerBoundries.right = true;
-  } else {
-    playerBoundries.right = false;
-  }
-  if(player1Rect.left <= gameWindowRect.left){
-    playerBoundries.left = true;
-  } else {
-    playerBoundries.left = false;
-  }
-  if(player1Rect.top <= gameWindowRect.top){
-    playerBoundries.top = true;
-  } else {
-    playerBoundries.top = false;
-  }
-  if(player1Rect.bottom >= gameWindowRect.bottom){
-    playerBoundries.bottom = true;
-  } else {
-    playerBoundries.bottom = false;
-  }
+// function checkBoundries(){
+//   let player1Rect = player1.getBoundingClientRect();
+//   if(player1Rect.right >= gameWindowRect.right){
+//     playerBoundries.right = true;
+//   } else {
+//     playerBoundries.right = false;
+//   }
+//   if(player1Rect.left <= gameWindowRect.left){
+//     playerBoundries.left = true;
+//   } else {
+//     playerBoundries.left = false;
+//   }
+//   if(player1Rect.top <= gameWindowRect.top){
+//     playerBoundries.top = true;
+//   } else {
+//     playerBoundries.top = false;
+//   }
+//   if(player1Rect.bottom >= gameWindowRect.bottom){
+//     playerBoundries.bottom = true;
+//   } else {
+//     playerBoundries.bottom = false;
+//   }
+// }
 
-}
-
-movePlayer();
-function movePlayer(){
-  if(button1.textContent === "resume") {
-    return;
-  }
+movePlayer(player1);
+function movePlayer(_player){
+  // if(button1.textContent === "resume") {
+  //   return;
+  // }
 
   const speed = 4;
-  const diagonalSpeed = 1 / (2*speed);
 
-  checkBoundries();
+  // checkBoundries();
   
   if (keysPressed.ArrowUp && !playerBoundries.top) {
-    player1Y -= speed;
+    _player.incrementPos(0,-speed);
   }
   if (keysPressed.ArrowDown && !playerBoundries.bottom) {
-    player1Y += speed;
+    _player.incrementPos(0,speed);
   }
   if (keysPressed.ArrowLeft && !playerBoundries.left) {
-    player1X -= speed;
+    _player.incrementPos(-speed,0);
   }
   if (keysPressed.ArrowRight && !playerBoundries.right) {
-    player1X += speed;
+    _player.incrementPos(speed,0);
   }
-  // // diagonal movement.
-  // if (keysPressed.ArrowUp && keysPressed.ArrowLeft && !playerBoundries.top && !playerBoundries.left) {
-  //   player1Y -= diagonalSpeed;
-  //   player1X -= diagonalSpeed;
-  // }
-  // if (keysPressed.ArrowUp && keysPressed.ArrowRight && !playerBoundries.top && !playerBoundries.right) {
-  //   player1Y -= diagonalSpeed;
-  //   player1X += diagonalSpeed;
-  // }
-  // if (keysPressed.ArrowDown && keysPressed.ArrowLeft && !playerBoundries.bottom && !playerBoundries.left) {
-  //   player1Y += diagonalSpeed;
-  //   player1X -= diagonalSpeed;
-  // }
-  // if (keysPressed.ArrowDown && keysPressed.ArrowRight && !playerBoundries.bottom && !playerBoundries.right) {
-  //   player1Y += diagonalSpeed;
-  //   player1X += diagonalSpeed;
-  // }
 
-  // update position.
-  player1.style.top = `${player1Y}px`;
-  // player1.style.top = player1Y + "px";
-  player1.style.left = `${player1X}px`;
-
-  gravityFall();
-
-  requestAnimationFrame(movePlayer);
+  requestAnimationFrame(() => {
+    movePlayer(_player);
+  });
+  
 }
 
 
-// gravity.
-function gravityFall(){
-  if(playerBoundries.bottom){
-    return;
-  }
-  player1Y += 0.4;
-  player1.style.top = `${player1Y}px`;
-}
+// // gravity.
+// function gravityFall(){
+//   if(playerBoundries.bottom){
+//     return;
+//   }
+//   player1Y += 0.4;
+//   player1.style.top = `${player1Y}px`;
+// }
